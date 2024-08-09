8dd601076902d9275e4f60533111505cd1afe07a
bao <bao.nq07591@sinhvien.hoasen.edu.vn>
2024-08-09 14:14:46 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,81 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import {CounterState} from "./ngrxcalculator/calculator.state";
+
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [FormsModule],
 })
 export class AppComponent {
-  title = 'calc';
+  display: string = '';
+  currentNumber: string = '';
+  operator: string | null = null;
+  previousNumber: string = '';
+
+  constructor(private store: Store<{ counter: CounterState }>) {}
+
+  appendNumber(number: string) {
+    this.currentNumber += number;
+    this.updateDisplay();
+  }
+
+  setOperator(operator: string) {
+    if (this.currentNumber === '') return;
+    if (this.previousNumber !== '') {
+      this.calculate();
+    }
+    this.operator = operator;
+    this.previousNumber = this.currentNumber;
+    this.currentNumber = '';
+  }
+
+  calculate() {
+    if (
+      this.currentNumber === '' ||
+      this.previousNumber === '' ||
+      this.operator === null
+    )
+      return;
+    const a = parseFloat(this.previousNumber);
+    const b = parseFloat(this.currentNumber);
+    let result: number;
+
+    switch (this.operator) {
+      case '+':
+        result = a + b;
+        break;
+      case '-':
+        result = a - b;
+        break;
+      case '*':
+        result = a * b;
+        break;
+      case '/':
+        result = a / b;
+        break;
+      default:
+        return;
+    }
+
+    this.currentNumber = result.toString();
+    this.previousNumber = '';
+    this.operator = null;
+    this.updateDisplay();
+  }
+
+  clear() {
+    this.display = '';
+    this.currentNumber = '';
+    this.previousNumber = '';
+    this.operator = null;
+  }
+
+  private updateDisplay() {
+    this.display = this.currentNumber;
+  }
 }
